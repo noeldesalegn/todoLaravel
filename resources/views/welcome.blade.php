af024ee397263cedfa9cd633eb8331d8765ac9f4
noeldesalegn <desalen2121@gmail.com>
2024-10-11 13:17:53 -0700

    2nd div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 
-<head>
+<head class="bg-gray-300">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App template</title>
@@ -11,7 +11,7 @@
 
 
 </head>
-<body>
+<body class="bg-gray-200">
     <div id="head" class="flex border-blue-800 border-t-2">
         <div class="w-full">
             <header class="flex bg-white justify-between h-20 border-b border-b-gray-200 items-center px-6">
